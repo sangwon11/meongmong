364a5708f1dc50ca49d52b93bbb27f26e295dd98
[SW7] 홍소현 <hshyeon86@gmail.com>
2023-11-02 12:21:44 +0000

    ✨ Feat 회원가입 페이지 UI

--- client/pages/signupPage/signupPage.js
+++ client/pages/signupPage/signupPage.js
@@ -1,1 +1,61 @@
 import '../../index.css';
+
+const name = document.getElementById('name');
+const telnum = document.getElementById('telnum');
+const email = document.getElementById('email');
+const pswd = document.getElementById('password');
+
+const btn = document.querySelector('form');
+let phone='';
+
+
+
+btn.addEventListener('submit', function(){
+        event.preventDefault();
+
+        postUser();   
+        swal("회원가입 완료", "반갑습니다!", "success").then(()=>{
+            location.href = '../loginPage/'
+        }); 
+       
+       
+        
+})
+
+
+const postUser = () =>{
+
+    fetch("", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+         body: JSON.stringify({
+            email: email.value,
+            password: pswd.value,
+            phone: telnum.value,
+            name: name.value,
+         }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+    
+}
+
+
+   // 숫자가 아닌 정규식
+   var replaceNotInt = /[^0-9]/gi;
+   const form = document.getElementById('telnumform');
+
+   telnum.addEventListener('keyup', function(){
+    telnum.value = telnum.value.replace(replaceNotInt,"");
+   } )
+
+   telnum.addEventListener('focusout', function(){
+    if(telnum.value){
+        if(telnum.value.match(replaceNotInt)){
+            telnum.value = telnum.value.replace(replaceNotInt,"");
+        }
+    }
+   })
+   
